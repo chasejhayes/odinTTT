31b472a2c426bfe8c8d3288e378fbbede93dd196
Chase Hayes <188492039+chasejhayes@users.noreply.github.com>
2026-03-29 13:50:27 -0500

    Add end game logic

--- javascript.js
+++ javascript.js
@@ -1,5 +1,12 @@
 // Tic Tac Toe
-// play1 turn. Makes a selection of a number 1-9 and object is updated to reflect
+// on click of box mark with x or o depending on turn counter
+// make player selection = to whatever box was clicked on
+// how to end game?
+// how many combinations of winning are there: 
+// 0 1 2, 0 3 6, 1 4 7, 2 5 8, 3 4 5, 6 7 8, 0 4 8, 2 4 6
+// eight combinations of winning values 
+// as the game goes on, store each chosen number in an array
+// once an array has any combination of those 8 three-digit numbers declare winner
 
 const gameBoard = [
     { position: 0, state: "unclicked", chosenBy: "notYet" },
@@ -14,13 +21,14 @@ const gameBoard = [
 
 ];
 
-const playerOne = {
+const playerOneNums = [
 
-}
+]
 
-const playerTwo = {
+const playerTwoNums = [
 
-}
+]
+
 
 let turnCounter = 0;
 function displayPlayerTurn() {
@@ -33,22 +41,44 @@ function displayPlayerTurn() {
     turnCounter++
 }
 
+
+
 function turn(playerSelection){
     let index = gameBoard.findIndex(item => item.position === playerSelection);
     (gameBoard[index].state = "clicked");
     if (turnCounter % 2 == 0){
-        (gameBoard[index].chosenBy = "PlayerOne") 
+        (gameBoard[index].chosenBy = "PlayerOne");
+        playerOneNums.push(playerSelection);
     } else {
-        gameBoard[index].chosenBy = "PlayerTwo" 
+        gameBoard[index].chosenBy = "PlayerTwo";
+        playerTwoNums.push(playerSelection);
     }
+
     turnCounter++;
 }
 
-turn(4);
-turn(8);
+// 0 1 2, 0 3 6, 1 4 7, 2 5 8, 3 4 5, 6 7 8, 0 4 8, 2 4 6
+
+function endGame(){
+    if (playerOneNums.includes((0, 1, 2) || (0, 3, 6) || (1 ,4, 7) || (2, 5, 8) || (3, 4, 5) || (6, 7, 8) || (0, 4, 8) || (2 ,4, 6))){
+        return console.log("Player One Wins")
+    } else if (playerTwoNums.includes((0, 1, 2) || (0, 3, 6) || (1 ,4, 7) || (2, 5, 8) || (3, 4, 5) || (6, 7, 8) || (0, 4, 8) || (2 ,4, 6))){
+        return console.log("Player One Wins")
+}}
+
+
 turn(0);
-turn(1)
+console.log(playerOneNums);
+console.log(playerTwoNums);
+turn(8);
+turn(1);
+turn(6);
+turn(2)
+console.log(playerOneNums);
+console.log(playerTwoNums);
+
 console.log(gameBoard)
+endGame()
 
 
 // function playgame() {
